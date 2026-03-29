cc768ef07f2e76a6836bc07b16d99569785e64c9
Glitch-500 <ramanand.24bai10427@vitbhopal.ac.in>
2026-03-29 22:44:56 +0530

    Update DatabaseManager.java

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -3,7 +3,7 @@ public class DatabaseManager {
     public static void saveToDB(String type, double impact) {
         System.out.println("Connecting to database...");
         System.out.println("Data Successfully Synced to MySQL: " + type + " - " + impact + "kg");
-    }
+    } 
 } 
  
  
