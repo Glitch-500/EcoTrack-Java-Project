c59b56f78cb3b3121129448e45e07204035b8e73
Glitch-500 <ramanand.24bai10427@vitbhopal.ac.in>
2026-03-29 22:43:35 +0530

    Update EcoTipThread.java

--- EcoTipThread.java
+++ EcoTipThread.java
@@ -2,7 +2,7 @@ public class EcoTipThread extends Thread {
     public void run() {
         String[] tips = {"Switch to LEDs!", "Unplug idle electronics.", "Walk for short distances."};
         try {
-            while (true) {
+            while (true) { 
                 System.out.println("\n[TIP]: " + tips[(int)(Math.random()*3)]);
                 Thread.sleep(15000); // Shows a tip every 15 seconds
             }
